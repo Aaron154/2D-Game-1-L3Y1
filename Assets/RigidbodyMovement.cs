387e470c3c54dac3d0d5c55173c7ffeb4587faab
ATA <aaron.bailey.168@accesscreative.ac.uk>
2024-11-26 11:25:10 +0000

    Added Player Movement/Wall HitBoxes

--- Assets/RigidbodyMovement.cs
+++ Assets/RigidbodyMovement.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))] //Tell Unity to add theses components to the gameobject this code is attached to.
 [RequireComponent(typeof(BoxCollider2D))] //We will still need to tweak some of the settings.
  public class RigidbodyMovement : MonoBehaviour
+ 
 {   
+    Animator animator;
    Rigidbody2D rb2d;
    public float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
@@ -16,6 +19,9 @@ using UnityEngine;
     {
         float moveInputX = Input.GetAxisRaw("Horizontal");
         float moveInputY = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("InputX", moveInputX);
+        animator.SetFloat("InputY", moveInputY);
 
         // Normalize the vector so that we don't move faster when moving diagonally.
         Vector2 moveDirection = new Vector2(moveInputX, moveInputY);
