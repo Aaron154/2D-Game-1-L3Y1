f627d31128e24dc471e136fcaad7cd21d5e5f900
ATA <aaron.bailey.168@accesscreative.ac.uk>
2025-01-28 11:23:16 +0000

    Added UI (Pause Menu)

---
+++ Assets/Scripts/UImanager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+    public GameObject pauseMenuUI; // Reference for the UI to enable/disable
+    public static bool isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) // Key to pause the game
+        {
+            if (isPaused) ResumeGame();     //If game is paused, resume game
+            else PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quitting The Game");
+        Application.Quit();     // Only works in games. Not the editor
+    }
+}
