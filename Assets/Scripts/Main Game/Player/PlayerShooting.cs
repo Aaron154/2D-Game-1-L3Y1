88bfbc35d1f72e368222121265b8abe92d06003e
ATA <aaron.bailey.168@accesscreative.ac.uk>
2025-02-25 10:12:02 +0000

    Sound Started

--- Assets/Scripts/Main Game/Player/PlayerShooting.cs
+++ Assets/Scripts/Main Game/Player/PlayerShooting.cs
@@ -9,10 +9,22 @@ public class PlayerShooting : MonoBehaviour
     public Transform bulletSpawnPoint;
 
     public float bulletSpeed = 20f;
+    float attackRate = 2f;
+
+    AudioSource audioSource;
+    public AudioClip attacksound;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (UIManager.isPaused)
+        return;
+
         RotateBulletSpawnPointTowardsMouse();
         if(Input.GetButtonDown("Fire1"))
         {
@@ -33,12 +45,14 @@ public class PlayerShooting : MonoBehaviour
     }
     void Shoot()
     {
+        lastTimeShot = Time.time;
+
+
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, firePointRotation.rotation);
-
+        bullet.transform.rotation = Quaternion.Euler(0, 0, firePointRotation.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
         rb.velocity = firePointRotation.right * bulletSpeed;
-
+        audioSource.PlayOneShot(attackSound);
         Destroy(bullet, 10f);
 
     }
