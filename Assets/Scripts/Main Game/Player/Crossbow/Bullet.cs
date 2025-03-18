cf115ed8e4ae7360f2c981d04fb712cfa7f10627
ATA <aaron.bailey.168@accesscreative.ac.uk>
2025-03-18 10:36:41 +0000

    Fixed Bullet Spawn

--- Assets/Scripts/Main Game/Player/Crossbow/Bullet.cs
+++ Assets/Scripts/Main Game/Player/Crossbow/Bullet.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public int damage = 4;
+   public int damage = 4;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //ignore objects with tag attackpoint
+
+        if (collision.CompareTag("AttackPoint"))
+            return;
+
         if (collision.CompareTag("Enemy"))
         {
             collision.GetComponent<Health>()?.TakeDamage(damage);
